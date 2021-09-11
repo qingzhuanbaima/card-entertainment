0fe94884732672516bc769c244052f870f242ffd
liuxuejin <1975133833@qq.com>
2021-09-11 13:18:54 +0800

    no message

---
+++ src/components/img-container.vue
@@ -0,0 +1,83 @@
+<template>
+	<div class="img-container">
+
+		<slot name="left-text" class="left-text"></slot>
+
+		<div class="pohto-frame">
+			<slot name="photo"></slot>
+			<img src="../assets/bgimg/frame_b.png" class="frame-top">
+			<img src="../assets/bgimg/frame_b.png" class="frame-right">
+			<img src="../assets/bgimg/frame_b.png" class="frame-bottom">
+			<img src="../assets/bgimg/frame_b.png" class="frame-left">
+		</div>
+
+		<slot name="right-text"></slot>
+
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "phpto-frame",
+		data() {
+			return {
+
+			}
+		}
+	}
+</script>
+
+<style>
+	.img-container {
+		display: flex;
+	}
+
+	.pohto-frame {
+		width: 380px;
+		height: 428px;
+		position: relative;
+	}
+
+	.frame-top {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 20px;
+		transform: rotateZ(180deg);
+	}
+
+	.frame-right {
+		position: absolute;
+		right: 0;
+		top: 0;
+		width: 428px;
+		height: 20px;
+		transform: rotateZ(-90deg) translateX(-50%) translateY(204px);
+	}
+
+	.frame-bottom {
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+		height: 20px;
+	}
+
+	.frame-left {
+		position: absolute;
+		left: 0;
+		top: 0;
+		width: 428px;
+		height: 20px;
+		transform: rotateZ(90deg) translateX(50%) translateY(204px);
+	}
+
+	.right-text {
+		width: 350px;
+	}
+
+	.left-text {
+		width: 350px;
+	}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,8 +11,9 @@
 							<img src="../assets/game_img/heartstone.gif">
 						</template>
 						<template v-slot:left-text>
-							<div>
-								<h2>炉石传说</h2>
+							<div class="text-container">
+								<span>炉石传说</span>
+								<img class="game-icon" src="../assets/game_img/hearthStone.png">
 								<p>
 									《炉石传说》（英语：Hearthstone，香港和台湾译作“炉石战记”，香港和台湾旧译“炉石战记：魔兽英雄传”，中国大陆旧译“炉石传说：魔兽英雄传”）是暴雪娱乐发行的一款集换式卡牌游戏[2][3]。由暴雪员工RobPardo在2013年3月的PAX2013公布。[4]游戏在2013年夏进入Beta测试。2014年1月24日进入公测阶段[5]。2014年3月13日，欧、亚、美三个服务器正式运营，中国服务器也于3月15日正式运营。该游戏是一款免费游戏，故事背景基于魔兽系列的世界观，玩家可以另行购买卡牌包
 								</p>
@@ -24,8 +25,9 @@
 							<img src="../assets/game_img/heartstone.gif">
 						</template>
 						<template v-slot:left-text>
-							<div>
-								<h2>炉石传说</h2>
+							<div class="text-container">
+								<span>炉石传说</span>
+								<img class="game-icon" src="../assets/game_img/hearthStone.png">
 								<p>
 									《炉石传说》（英语：Hearthstone，香港和台湾译作“炉石战记”，香港和台湾旧译“炉石战记：魔兽英雄传”，中国大陆旧译“炉石传说：魔兽英雄传”）是暴雪娱乐发行的一款集换式卡牌游戏[2][3]。由暴雪员工RobPardo在2013年3月的PAX2013公布。[4]游戏在2013年夏进入Beta测试。2014年1月24日进入公测阶段[5]。2014年3月13日，欧、亚、美三个服务器正式运营，中国服务器也于3月15日正式运营。该游戏是一款免费游戏，故事背景基于魔兽系列的世界观，玩家可以另行购买卡牌包
 								</p>
@@ -39,8 +41,9 @@
 							<img src="../assets/game_img/heartstone.gif">
 						</template>
 						<template v-slot:right-text>
-							<div>
-								<h2>炉石传说</h2>
+							<div class="text-container">
+								<span>炉石传说</span>
+								<img class="game-icon" src="../assets/game_img/hearthStone.png">
 								<p>
 									《炉石传说》（英语：Hearthstone，香港和台湾译作“炉石战记”，香港和台湾旧译“炉石战记：魔兽英雄传”，中国大陆旧译“炉石传说：魔兽英雄传”）是暴雪娱乐发行的一款集换式卡牌游戏[2][3]。由暴雪员工RobPardo在2013年3月的PAX2013公布。[4]游戏在2013年夏进入Beta测试。2014年1月24日进入公测阶段[5]。2014年3月13日，欧、亚、美三个服务器正式运营，中国服务器也于3月15日正式运营。该游戏是一款免费游戏，故事背景基于魔兽系列的世界观，玩家可以另行购买卡牌包
 								</p>
@@ -52,8 +55,9 @@
 							<img src="../assets/game_img/heartstone.gif">
 						</template>
 						<template v-slot:right-text>
-							<div>
-								<h2>炉石传说</h2>
+							<div class="text-container">
+								<span>炉石传说</span>
+								<img class="game-icon" src="../assets/game_img/hearthStone.png">
 								<p>
 									《炉石传说》（英语：Hearthstone，香港和台湾译作“炉石战记”，香港和台湾旧译“炉石战记：魔兽英雄传”，中国大陆旧译“炉石传说：魔兽英雄传”）是暴雪娱乐发行的一款集换式卡牌游戏[2][3]。由暴雪员工RobPardo在2013年3月的PAX2013公布。[4]游戏在2013年夏进入Beta测试。2014年1月24日进入公测阶段[5]。2014年3月13日，欧、亚、美三个服务器正式运营，中国服务器也于3月15日正式运营。该游戏是一款免费游戏，故事背景基于魔兽系列的世界观，玩家可以另行购买卡牌包
 								</p>
@@ -78,6 +82,30 @@
 	}
 </script>
 <style lang="scss">
+	@keyframes shake {
+
+		10%,
+		90% {
+			transform: translate3d(-1px, 0, 0);
+		}
+
+		20%,
+		80% {
+			transform: translate3d(2px, 0, 0);
+		}
+
+		30%,
+		50%,
+		70% {
+			transform: translate3d(-4px, 0, 0);
+		}
+
+		40%,
+		60% {
+			transform: translate3d(4px, 0, 0);
+		}
+	}
+
 	.home {
 		// position: relative;
 		font-family: "楷体", "楷体_GB2312";
@@ -106,5 +134,37 @@
 		display: flex;
 		width: 60%;
 		margin: 0 auto;
+
+		h2 {
+			display: inline-block;
+			// margin: 30px 0;
+			height: 40px;
+			width: 100px;
+		}
+
+		span {
+			display: block;
+			height: 40px;
+			// width: 150px;
+			padding: 20px 0;
+			line-height: 40px;
+			float: left;
+			font-size: 30px;
+		}
+
+		.game-icon {
+			display: inline-block;
+			width: 40px;
+			height: 40px;
+			padding: 20px 0;
+			margin-left: 10px;
+		}
+	}
+
+	.img-container:hover .game-icon {
+		animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+		transform: translate3d(0, 0, 0);
+		backface-visibility: hidden;
+		perspective: 1000px;
 	}
 </style>
